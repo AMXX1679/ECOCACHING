6a628130ea6f183fe2dd55e6eb496f83711a9a9b
AMXX1679 <arnemalek@gmail.com>
2023-02-04 19:32:28 +0100

    ads
    aayjd ase rier grt hrt h

---
+++ ecocaching/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="container">
+
+      <h2 style="font-family: Arial, Verdana; right: 1445px;"><a href="./poinst.html">punkte</a></h2>
+
+      <h1 id="überschrift" style="font-family: Arial, Verdana;">ECOCACHING</h1>
+
+    </div>
+  );
+}
+
+export default App;
